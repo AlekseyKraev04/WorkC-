0811d2e1c1b48cae7c9123fc3d4609e296beab20
Aleksey Kraev <kraev.81@mail.ru>
2022-07-05 13:52:37 +0300

    Task _08

--- Task_08/Program.cs
+++ Task_08/Program.cs
@@ -3,4 +3,15 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Cons
+Console.WriteLine("Нужно определить четные числа от 1 до N:");
+
+Console.WriteLine("Вводим целое число N:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+String result = string.Empty;
+
+for (int i = 2; i <= num1; i += 2)
+{
+    result = result + " " + i;
+}
+Console.WriteLine("Четные числа от 0 до N: " +result);
